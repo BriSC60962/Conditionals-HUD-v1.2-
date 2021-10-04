a36024fce1189ec314fedc642b6e4275748d28ad
Brianna Chisholm <70978684+BriSC60962@users.noreply.github.com>
2021-10-04 14:00:14 -0300

    Update Program.cs

--- Conditionals (HUD v1.2)/Program.cs
+++ Conditionals (HUD v1.2)/Program.cs
@@ -10,27 +10,74 @@ namespace Conditionals__HUD_v1._2_
     {
         static int weapon = 0;
         static int health = 100;
+        static string healthStatus;
+        static int damage;
+        static int hp;
         static void Main(string[] args)
         {
+            ShowHUD();
+            damage =10;
+            TakeDamage(damage);
+            //hp = 200;
+            //Heal(hp);
             ShowHUD();
             Console.ReadKey(true);
         }
         static void ShowHUD()
+            
         {
-            Console.WriteLine("Weapon: " + weapon);
-            Console.WriteLine("Health: " + health);
+            // Console.WriteLine("Weapon: " + weapon);
+            // Console.WriteLine("Health: " + health);
+
+            
+            if (health == 100)
+            {
+                healthStatus = "Perfect Health";
+            }
+            else if (health >= 100)
+            {
+                healthStatus = "Healthy";
+            }
+            else if (health >= 75)
+            {
+                healthStatus = "Hurt";
+            } 
+            else if (health >= 50)
+            {
+                healthStatus = "Badly Hurt";
+            }
+            else if (health >=10)
+            {
+                healthStatus = "Imminent Danger";
+            }
+            else
+            {
+                healthStatus = "Dead";
+            }
+            Console.WriteLine("Health: " + health + " Health Status: " + healthStatus);
         }
         static void ChangeWeapon(int weaponPickUp)
         {
+            weapon = weaponPickUp;
 
+            if (weapon == 0)
+            {
+                
+            }
         }
         static void TakeDamage(int damage)
         {
             health = health - damage;
+            if (health < 0)
+            { 
+                health = 0; 
+            }
         }
         static void Heal(int hp)
         {
             health = health + hp;
+            if (health > 100)
+                health = 100;
         }
     }
 }
